d7bebaeae65a9a8c61e05b8d61ce4b28f774fba9
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-12-13 22:33:08 +0100

    function

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -10,7 +10,7 @@ function startGame() {
     if (sum < 21) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Woohoo! You've got Blackajack"
+        message = "You've got Blackajack"
         hasBlackJack = true
     } else {
         message = "You're out of the game!"

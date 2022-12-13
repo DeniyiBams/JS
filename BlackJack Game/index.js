cfdc7ea1f2a35ed5a8cde6fdaee8370aae64ddb8
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-12-13 22:30:13 +0100

    startGame function added

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,30 +1,3 @@
-// document.getElementById("count-el").innerText = 6
-// let count = 0
-// let countEl = document.getElementById("count-el")
-// let saveEl = document.getElementById("save-el")
-// let entry = "Previous entries: "
-// function increment() {
-//     count = count + 1
-//     countEl.textContent = count
-// }
-// function reset() {
-//     count = 0
-//     countEl.textContent = count
-// }
-
-// function save() {
-//     let entry = count + " - "
-//     saveEl.textContent += entry
-//     countEl.textContent = 0
-//     count = 0
-// }
-
-// let err = document.getElementById("error")
-// let errrorMessage = "Something went wrong, please try again"
-// function purchase() {
-//     err.textContent = errrorMessage
-// }
-
 let firstCard = 10
 let secondCard = 2
 let sum = firstCard + secondCard

2c21ca0bc9e1c0dfefd6acd8398c5626a18e097e
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-12-16 00:35:40 +0100

    fixed

---
+++ README.md
@@ -0,0 +1,6 @@
+BLACK JACK GAME
+
+Returns two numbers/ 2 cards which ranges from 1-13(inclusive):
+    - If the sum is less than 21, the player get the chance to draw a new card.
+    - If the sum is exactly 21, the player wins.
+    - If the sum is above 21, the player is out of the game.
---
+++ index.js
@@ -0,0 +1,63 @@
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
+
+let player = {
+    name: "DeniyiBams",
+    chips: 145
+}
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1 // generates a number from 1-13
+    if (randomNumber === 1){
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+    return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    sum = firstCard + secondCard
+
+    cards = [firstCard, secondCard]
+    renderGame()
+}
+
+function renderGame() {
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackajack"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardEl.textContent = "Sum: "
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent += cards[i] + " "
+    }
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false)  {
+        let nextCard = getRandomCard()
+        sum += nextCard
+        cards.push(nextCard)
+        renderGame()
+    }
+}
